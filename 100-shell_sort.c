0d440482f6c3ec83dd67583419e0e1a00ac31141
euGenius29 <angloweugenious29@gmail.com>
2024-01-24 05:30:53 -0800

    adding the first advanced task

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ * shell_sort - Sorts an array of integers in ascending order using Shell sort
+ *              algorithm with Knuth sequence
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+	gap = 1;
+
+	while (gap <size)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		gap = (gap - 1) / 3;
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+
+			array[j] = temp;
+		}
+		print_array(array, size);
+	}
+}
